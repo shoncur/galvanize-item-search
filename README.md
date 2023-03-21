8fb7078d84dc1219eaaad9b7baefe76762a3e1c5
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2023-03-21 09:50:32 -0600

    changed method of reading files/deprecated old app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # galvanize-item-search
+## Shaun Currie
 This application should be able to search through pdf and word documents in order to find all item numbers within
--- item-search.py
+++ item-search.py
@@ -1,47 +1,32 @@
-# Author: Shaun Currie
-# Date: 2023-01-24
-
-# Goal:
-#   To create a simple application that takes in a document, and outputs a list of item numbers
-
-from tkinter import *
+import PyPDF2
+import textract
 from tkinter import filedialog
-from tkinter import ttk
-from tkinter import Text
 
-import re
+# reader = PyPDF2.PdfReader('100-00040-013_B Software Design Specification.pdf')
+# pages = len(reader.pages)
+# for i in range(pages):
+#     text = reader.pages[i].extract_text()
+#     print(text)
 
-from PyPDF2 import PdfReader
+# text = textract.process('01RegexTest.txt')
+# print(text)
 
-# This is called when the user clicks the "browse" button
-def UploadAction(event=None):
+def UploadFile(event=None):
     filename = filedialog.askopenfilename()
-    print('Selected:', filename)    
-    ttk.Label(mainframe, text=filename).grid(column=1, row=2, sticky=(E))
-    FindItems(filename)
+    try:
+        reader = PyPDF2.PdfReader(filename)
+        pages = len(reader.pages)
+        text = ""
+        for i in range(pages):
+            text += reader.pages[i].extract_text()
+    except:
+        try:
+            text = textract.process(filename)
+        except:
+            print('invalid file')
+        else:
+            print(text)
+    else:
+        print(text)
 
-# Find all instances of an item in the document
-def FindItems(filename):
-    textfile = open(filename, 'r')
-    filetext = textfile.read()
-    textfile.close()
-    matches = re.findall("[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]", filetext)
-    listofitems = list(dict.fromkeys(matches))
-    ttk.Label(mainframe, text=listofitems).grid(column=1, row=3)
-
-# Instantiate tkinter
-root = Tk()
-root.title("Item Number Search")
-
-# Setting up the grid for adding elements to the application
-mainframe = ttk.Frame(root, padding="12 12 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, E, W, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-# "Choose a File: " label and "Browse" button
-ttk.Label(mainframe, text="Choose a File: ").grid(column=1, row=1, sticky=(W))
-ttk.Button(mainframe, text="Browse", command=UploadAction).grid(column=2, row=1, sticky=(W))
-
-# Render the GUI
-root.mainloop()
+UploadFile()

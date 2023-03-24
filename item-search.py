18b0e1b30309f65d55411ed5265d52d9fa85fafb
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2023-03-24 14:02:43 -0600

    gui started, allows for successful browse

--- item-search.py
+++ item-search.py
@@ -1,32 +1,76 @@
 import PyPDF2
-import textract
+import docx
+import re
 from tkinter import filedialog
+from PyQt5.QtCore import QSize, Qt, pyqtSlot
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+import sys
 
-# reader = PyPDF2.PdfReader('100-00040-013_B Software Design Specification.pdf')
-# pages = len(reader.pages)
-# for i in range(pages):
-#     text = reader.pages[i].extract_text()
-#     print(text)
-
-# text = textract.process('01RegexTest.txt')
-# print(text)
-
+# allows the user to choose a file
 def UploadFile(event=None):
     filename = filedialog.askopenfilename()
+    # try for pdf
     try:
         reader = PyPDF2.PdfReader(filename)
         pages = len(reader.pages)
-        text = ""
+        pdfText = ""
         for i in range(pages):
-            text += reader.pages[i].extract_text()
+            pdfText += reader.pages[i].extract_text()
+        pdfText = pdfText.replace(" ", "")
     except:
+        # try for docx NOT WORKING CURRENTLY
         try:
-            text = textract.process(filename)
+            doc = docx.Document(filename)
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)
+            docText = '\n'.join(fullText)
+            docText = docText.replace(" ", "")
         except:
             print('invalid file')
         else:
-            print(text)
+            print(docText)
+            print('-----------------')
+            FindItems(docText)
     else:
-        print(text)
+        print(pdfText)
+        print('-----------------')
+        FindItems(pdfText)
 
-UploadFile()
+# find all instances of an item in the document provided
+def FindItems(text):
+    try:
+        items = re.findall('[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]', text)
+        listofitems = list(dict.fromkeys(items))
+    except:
+        print('can not find valid item #\'s')
+    else:
+        print(listofitems)
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = ('Item Search')
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setMinimumSize(QSize(400,300))
+
+        browseButton = QPushButton('Browse', self)
+        browseButton.setToolTip('Choose a file')
+        browseButton.clicked.connect(self.on_click)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        UploadFile()
+
+if __name__ == '__main__':
+    # application instance
+    app = QApplication(sys.argv)
+
+    ex = App()
+    sys.exit(app.exec_())

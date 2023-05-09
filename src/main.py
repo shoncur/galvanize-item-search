8d7bca1a31124fe08fe649be7ab4a9e63a958a81
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2023-05-09 14:20:22 -0600

    renamed item-search.py, moved patterns to dif file

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import os
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QListWidget, QRadioButton, QMessageBox, QDialog
 from PyQt5.QtGui import QPixmap, QIcon
 from collections import OrderedDict
+from patterns import list_of_patterns
 
 class PDFReader(QWidget):
     def __init__(self):
@@ -63,84 +64,12 @@ class PDFReader(QWidget):
         # Open PDF and extract text
         pdf_document = fitz.open(file_name)
 
-        # List of regex patterns for various number formats
-        patterns = [
-            r'\d{3}-\d{5}-\d{3}',
-            r'\d{3}-\d{5}-\d{2}(?!\d)',
-            r'\d{3}-\d{5}(?!-)',
-            r'(?<!(\d|-))\d{2}-\d{5}',
-            r'ECO-\d{2}-\d{5}',
-            r'ACM-\d{5}-\d{2}',
-            r'ACM-\d{5}(?!-)',
-            r'CER-\d{5}-\d{2}',
-            r'CER-\d{5}(?!-)',
-            r'CEP-\d{5}-\d{2}',
-            r'CEP-\d{5}(?!-)',
-            r'CSP-\d{5}-\d{2}',
-            r'CSP-\d{5}(?!-)',
-            r'CSR-\d{5}-\d{2}',
-            r'CSR-\d{5}(?!-)',
-            r'CLN-\d{5}-\d{2}',
-            r'CLN-\d{5}(?!-)',
-            r'DDP-\d{5}',
-            r'(?<!\w)DP-\d{4}',
-            r'DCD-\d{5}-\d{2}',
-            r'DCD-\d{5}(?!-)',
-            r'DHF-\d{5}-[A-Z0-9]{2}',
-            r'DHF-\d{5}(?!-)',
-            r'DMR-\d{5}-[A-Z0-9]{2}',
-            r'DMR-\d{5}(?!-)',
-            r'DOT-\d{5}-\d{2}',
-            r'DOT-\d{5}(?!-)',
-            r'EDR-\d{5}(?!-)',
-            r'FRM-\d{5}(?!-)',
-            r'LRP-\d{5}(?!-)',
-            r'LRR-\d{5}(?!-)',
-            r'LHR-\d{5}(?!-)',
-            r'LHR-\d{5}-\d{2}',
-            r'LHR-\d{5}-\d{3}',
-            r'MPI-\d{5}(?!-)',
-            r'MKG-\d{5}(?!-)',
-            r'MKG-\d{5}-\d{2}',
-            r'MA-\d{5}(?!-)',
-            r'PCP-\d{5}-\d{2}',
-            r'PCR-\d{5}(?!-)',
-            r'PMSP-\d{5}(?!-)',
-            r'PMSR-\d{5}(?!-)',
-            r'PSUR-\d{5}(?!-)',
-            r'QPL-\d{5}(?!-)',
-            r'RAD-\d{5}(?!-)',
-            r'RSK-\d{5}(?!-)',
-            r'SLS-\d{5}(?!-)',
-            r'SWP-\d{5}(?!-)',
-            r'SOP-\d{5}(?!-)',
-            r'STM-\d{5}(?!-)',
-            r'TFN-\d{5}(?!-)',
-            r'TFN-\d{5}-\d{2}',
-            r'TP-\d{5}(?!-)',
-            r'TP-\d{5}-\d{2}',
-            r'TR-\d{5}(?!-)',
-            r'TR-\d{5}-\d{2}',
-            r'WRK-\d{5}(?!-)',
-            r'FAB-\d{5}-\d{3}-\d{3}',
-            r'BRD-\d{5}-\d{3}-\d{3}',
-            r'GTI-\d{5}-\d{2}',
-            r'EQP-\d{5}',
-            r'PRT-\d{5}-\d{3}-\d{3}',
-            r'PRT-\d{5}-\d{3}',
-            r'LBL-\d{5}-\d{3}',
-            r'OTS-\d{5}-\d{3}',
-            r'SCH-\d{5}-\d{3}-\d{3}',
-            r'SW-\d{5}-\d{3}-\d{3}',
-            r'TFX-\d{5}-\d{3}-\d{3}'
-        ]
-
         matches = []
         for page_num in range(pdf_document.page_count):
             page = pdf_document.load_page(page_num)
             page_text = page.get_text().replace(" ", "").replace("\n", "")
             print(page_text)
-            for pattern in patterns:
+            for pattern in list_of_patterns:
                 page_matches = re.findall(pattern, page_text)
                 if page_matches:
                     matches.extend([(match, pattern, page_num + 1) for match in page_matches])
@@ -150,11 +79,13 @@ class PDFReader(QWidget):
                 for match, pattern, page_num in matches:
                     item = f'Page: {page_num} | {match}'
                     self.list_widget.addItem(item)
+                    print(f"'{match}'")
             else:
                 #unique_matches = list(OrderedDict.fromkeys([match for match, _, _ in matches]))
                 unique_matches = sorted(list(set([match for match, _, _ in matches])))
                 for match in unique_matches:
                     self.list_widget.addItem(f'{match}')
+                    print(f"'{match}'")
             self.download_button.setEnabled(True)
         else:
             self.list_widget.addItem('Nothing found')

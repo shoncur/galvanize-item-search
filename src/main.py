503a6fe1b5725674a0501028573610b6e69322e9
shoncur <shaundcurrie@gmail.com>
2023-12-07 16:00:44 -0800

    Big UI update showing both uniques and dupes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import re
 import os
 import csv
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QProgressDialog, QLineEdit, QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QListWidget, QRadioButton, QMessageBox, QDialog
+from PyQt5.QtWidgets import QProgressDialog, QLineEdit, QApplication, QWidget, QGroupBox, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QLabel, QFileDialog, QListWidget, QRadioButton, QMessageBox, QDialog
 from PyQt5.QtGui import QPixmap, QIcon
 from collections import OrderedDict
 from patterns import list_of_patterns, supported_item_numbers
@@ -80,44 +80,68 @@ class PDFReader(QWidget):
         script_dir = os.path.dirname(os.path.abspath(__file__))
 
         # Create widgets
-        self.browse_button = QPushButton('Browse', self)
+        self.browse_button = QPushButton('Choose a File...', self)
         self.browse_button.clicked.connect(self.browse_pdf)
-        self.label = QLabel('No PDF file selected', self)
-        self.list_widget = QListWidget(self)
-        self.page_number_label = QLabel('Show duplicate items:', self)
-        self.show_page_numbers_radio = QRadioButton('Yes', self)
-        self.show_page_numbers_radio.setChecked(False)
-        self.show_page_numbers_radio.toggled.connect(self.toggle_page_numbers)
+        self.browse_label = QLabel('No PDF file selected', self)
+        self.browse_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.unique_label = QLabel('Unique Item Numbers:', self)
+        self.list_widget_unique = QListWidget(self)
+        self.duplicate_label = QLabel('Duplicate Item Numbers:', self)
+        self.list_widget_duplicates = QListWidget(self)
+
+        # self.page_number_label = QLabel('Show duplicate items:', self)
+        # self.show_page_numbers_radio = QRadioButton('Yes', self)
+        # self.show_page_numbers_radio.setChecked(False)
+        # self.show_page_numbers_radio.toggled.connect(self.toggle_page_numbers)
         self.supported_numbers_button = QPushButton('Show Supported Item Numbers', self)
         self.supported_numbers_button.clicked.connect(self.supported_numbers_window)
-        self.download_button = QPushButton('Download', self)
+        self.download_button = QPushButton('Download Results', self)
         self.download_button.clicked.connect(self.download_matches)
         self.download_button.setEnabled(False)
 
         # Set up layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.browse_button)
-        layout.addWidget(self.label)
-        layout.addWidget(self.page_number_label)
-        layout.addWidget(self.show_page_numbers_radio)
-        layout.addWidget(self.supported_numbers_button)
-        layout.addWidget(self.list_widget)
-        layout.addWidget(self.download_button)
+        layout = QGridLayout()
+
+        layout.addWidget(self.browse_button, 0, 0)
+        layout.addWidget(self.supported_numbers_button, 0, 1)
+        layout.addWidget(self.browse_label, 1, 0, 1, 2)
+        # left_layout.addWidget(self.page_number_label)
+        # left_layout.addWidget(self.show_page_numbers_radio)
+        
+
+        # GroupBox for the item numbers
+        group_box = QGroupBox()
+        group_layout = QGridLayout()
+        group_layout.addWidget(self.unique_label, 0, 0, Qt.AlignmentFlag.AlignTop)
+        group_layout.addWidget(self.duplicate_label, 0, 1, Qt.AlignmentFlag.AlignTop)
+        group_layout.addWidget(self.list_widget_unique, 1, 0)
+        group_layout.addWidget(self.list_widget_duplicates, 1, 1)
+        group_box.setLayout(group_layout)
+
+        # Change background colour for GroupBox
+        group_box.setStyleSheet("QGroupBox { background-color: #e3e3e3; border: 1px solid #aaaaaa; padding-top: 2px; }")
+
+        layout.addWidget(group_box, 3, 0, 1, 2)
+
+        layout.addWidget(self.download_button, 4, 0, 1, 2)  
 
         self.setLayout(layout)
 
     def browse_pdf(self):
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open PDF File', '', 'PDF Files (*.pdf)')
         if file_name:
-            self.label.setText(f'Selected file: {file_name}')
+            self.browse_label.setText(f'Selected file: {file_name}')
             self.extract_numbers(file_name)
         else:
-            self.label.setText('No PDF file selected')
-            self.list_widget.clear()
+            self.browse_label.setText('No PDF file selected')
+            self.list_widget_unique.clear()
+            self.list_widget_duplicates.clear()
             self.download_button.setEnabled(False)
 
     def extract_numbers(self, file_name):
-        self.list_widget.clear()
+        self.list_widget_unique.clear()
+        self.list_widget_duplicates.clear()
 
         # Open PDF and extract text
         pdf_document = fitz.open(file_name)
@@ -134,55 +158,54 @@ class PDFReader(QWidget):
                     matches.extend([(match, pattern, page_num + 1) for match in cleaned_matches])
 
         if matches:
-            if self.show_page_numbers_radio.isChecked():
-                for match, pattern, page_num in matches:
-                    item = f'Page: {page_num} | {match}'
-                    self.list_widget.addItem(item)
+            for match, pattern, page_num in matches:
+                item = f'Page: {page_num} | {match}'
+                self.list_widget_duplicates.addItem(item)
+                print(f"'{match}'")
+
+            unique_matches = sorted(list(set([match for match, _, _ in matches])))
+            progress_dialog = QProgressDialog("Loading items...", "Cancel", 0, len(unique_matches), self)
+            progress_dialog.setWindowModality(Qt.WindowModal)
+            progress_dialog.setAutoReset(False)
+            progress_dialog.setAutoClose(False)
+            progress_dialog.setValue(0)
+            for index, match in enumerate(unique_matches):
+                progress_dialog.setValue(index)
+                QApplication.processEvents()
+                try: # Get the item from the items world in Arena
+                    item_url = f'{BASE_URL}/items?number={match}'
+                    item_headers = {'arena_session_id':f'{arena_session_id}', 'Content-Type':'application/json'}
+                    item_response = requests.get(item_url, headers=item_headers)
+                    item_lifecycle_phase = item_response.json()['results'][0]['lifecyclePhase']['name']
+                    item_revision = item_response.json()['results'][0]['revisionNumber']
+                    self.list_widget_unique.addItem(f'{match} || {item_lifecycle_phase} | Rev {item_revision}')
                     print(f"'{match}'")
-            else:
-                unique_matches = sorted(list(set([match for match, _, _ in matches])))
-                progress_dialog = QProgressDialog("Loading items...", "Cancel", 0, len(unique_matches), self)
-                progress_dialog.setWindowModality(Qt.WindowModal)
-                progress_dialog.setAutoReset(False)
-                progress_dialog.setAutoClose(False)
-                progress_dialog.setValue(0)
-                for index, match in enumerate(unique_matches):
-                    progress_dialog.setValue(index)
-                    QApplication.processEvents()
-                    try: # Get the item from the items world in Arena
-                        item_url = f'{BASE_URL}/items?number={match}'
-                        item_headers = {'arena_session_id':f'{arena_session_id}', 'Content-Type':'application/json'}
-                        item_response = requests.get(item_url, headers=item_headers)
-                        item_lifecycle_phase = item_response.json()['results'][0]['lifecyclePhase']['name']
-                        item_revision = item_response.json()['results'][0]['revisionNumber']
-                        self.list_widget.addItem(f'{match} || {item_lifecycle_phase} | Rev {item_revision}')
-                        print(f"'{match}'")
-                    except Exception as error: 
-                        try: # Try to get the item from the supplieritems world
-                            supplier_item_url = f'{BASE_URL}/supplieritems?number={match}'
-                            supplier_item_response = requests.get(supplier_item_url, headers=item_headers)
-                            supplier_item_guid = supplier_item_response.json()['results'][0]['guid']
-                            supplier_item_sourcing_url = f'{BASE_URL}/supplieritems/{supplier_item_guid}/sourcing'
-                            supplier_item_sourcing_response = requests.get(supplier_item_sourcing_url, headers=item_headers)
-                            try:
-                                linked_item = supplier_item_sourcing_response.json()['results'][0]['item']['number']
-                                linked_item_url = f'{BASE_URL}/items?number={linked_item}'
-                                linked_item_response = requests.get(linked_item_url, headers=item_headers)
-                                linked_item_revision = linked_item_response.json()['results'][0]['revisionNumber']
-                                linked_item_lifecycle_phase = linked_item_response.json()['results'][0]['lifecyclePhase']['name']
-                                self.list_widget.addItem(f'{match} | {linked_item} | {linked_item_lifecycle_phase} | Rev {linked_item_revision}')
-                            except:
-                                self.list_widget.addItem(f'{match} | Something went wrong, please check item # in Arena to verify the issue')
-                                print(f'{error}')
+                except Exception as error: 
+                    try: # Try to get the item from the supplieritems world
+                        supplier_item_url = f'{BASE_URL}/supplieritems?number={match}'
+                        supplier_item_response = requests.get(supplier_item_url, headers=item_headers)
+                        supplier_item_guid = supplier_item_response.json()['results'][0]['guid']
+                        supplier_item_sourcing_url = f'{BASE_URL}/supplieritems/{supplier_item_guid}/sourcing'
+                        supplier_item_sourcing_response = requests.get(supplier_item_sourcing_url, headers=item_headers)
+                        try:
+                            linked_item = supplier_item_sourcing_response.json()['results'][0]['item']['number']
+                            linked_item_url = f'{BASE_URL}/items?number={linked_item}'
+                            linked_item_response = requests.get(linked_item_url, headers=item_headers)
+                            linked_item_revision = linked_item_response.json()['results'][0]['revisionNumber']
+                            linked_item_lifecycle_phase = linked_item_response.json()['results'][0]['lifecyclePhase']['name']
+                            self.list_widget_unique.addItem(f'{match} | {linked_item} | {linked_item_lifecycle_phase} | Rev {linked_item_revision}')
                         except:
-                            self.list_widget.addItem(f'{match} | Item not found in Arena')
+                            self.list_widget_unique.addItem(f'{match} | Something went wrong, please check item # in Arena to verify the issue')
                             print(f'{error}')
-                    if progress_dialog.wasCanceled():
-                        break
-                progress_dialog.close()
+                    except:
+                        self.list_widget_unique.addItem(f'{match} | Item not found in Arena')
+                        print(f'{error}')
+                if progress_dialog.wasCanceled():
+                    break
+            progress_dialog.close()
             self.download_button.setEnabled(True)
         else:
-            self.list_widget.addItem('Nothing found')
+            self.list_widget_unique.addItem('Nothing found')
             self.download_button.setEnabled(False)
 
     def supported_numbers_window(self):
@@ -205,13 +228,13 @@ class PDFReader(QWidget):
         window.exec_()
 
     def toggle_page_numbers(self):
-        if self.label.text() != 'No PDF file selected':
-            self.extract_numbers(self.label.text().split(': ')[1])  # Re-extract and display matches based on new mode
+        if self.browse_label.text() != 'No PDF file selected':
+            self.extract_numbers(self.browse_label.text().split(': ')[1])  # Re-extract and display matches based on new mode
         # else:
         #     QMessageBox.warning(self, 'Error', 'Please select a PDF file first.', QMessageBox.Ok)     No need to currently display this error
 
     def download_matches(self):
-        if self.list_widget.count() == 0:
+        if self.list_widget_unique.count() == 0:
             QMessageBox.warning(self, 'Error', 'No matches found to download.', QMessageBox.Ok)
             return
 
@@ -220,33 +243,35 @@ class PDFReader(QWidget):
             if file_name.lower().endswith('.csv'):
                 with open(file_name, 'w', newline='') as file:
                     writer = csv.writer(file)
-                    if self.show_page_numbers_radio.isChecked():
-                        header = ['Page Number', 'Part Number']
-                        writer.writerow(header)
-                        # Write all matches with page numbers
-                        for i in range(self.list_widget.count()):
-                            item = self.list_widget.item(i).text()
-                            writer.writerow(item.split('|'))
-                            # writer.writerow(re.split(r'\||=', item))  # Split into cells if "|" or "=" is present
-                    else:
-                        header = ['Part Number', 'Galvanize Equivalent', 'Lifecycle Status', 'Revision']
-                        writer.writerow(header)
-                        # Write unique matches without page numbers
-                        unique_matches = list(OrderedDict.fromkeys([self.list_widget.item(i).text() for i in range(self.list_widget.count())]))
-                        for item in unique_matches:
-                            writer.writerow(item.split('|'))  # Split into cells if "|" is present
+
+                    unique_header = ['Part Number', 'Galvanize Equivalent', 'Lifecycle Status', 'Revision']
+                    writer.writerow(unique_header)
+                    # Write unique matches without page numbers
+                    unique_matches = list(OrderedDict.fromkeys([self.list_widget_unique.item(i).text() for i in range(self.list_widget_unique.count())]))
+                    for item in unique_matches:
+                        writer.writerow(item.split('|'))  # Split into cells if "|" is present
+
+                    separate_unique_duplicates = ['----------']
+                    writer.writerow(separate_unique_duplicates)
+
+                    duplicate_header = ['Page Number', 'Part Number']
+                    writer.writerow(duplicate_header)
+                    # Write all matches with page numbers
+                    for i in range(self.list_widget_duplicates.count()):
+                        item = self.list_widget_duplicates.item(i).text()
+                        writer.writerow(item.split('|'))
+                        # writer.writerow(re.split(r'\||=', item))  # Split into cells if "|" or "=" is present
+                    
             else:
                 with open(file_name, 'w') as file:
-                    if self.show_page_numbers_radio.isChecked():
-                        # Write all matches with page numbers
-                        for i in range(self.list_widget.count()):
-                            item = self.list_widget.item(i).text()
-                            file.write(f'{item}\n')
-                    else:
-                        # Write unique matches without page numbers
-                        unique_matches = list(OrderedDict.fromkeys([self.list_widget.item(i).text() for i in range(self.list_widget.count())]))
-                        for item in unique_matches:
-                            file.write(f'{item}\n')
+                    # Write unique matches without page numbers
+                    unique_matches = list(OrderedDict.fromkeys([self.list_widget_unique.item(i).text() for i in range(self.list_widget_unique.count())]))
+                    for item in unique_matches:
+                        file.write(f'{item}\n')
+                    # Write all matches with page numbers
+                    for i in range(self.list_widget_duplicates.count()):
+                        item = self.list_widget_duplicates.item(i).text()
+                        file.write(f'{item}\n')
 
             QMessageBox.information(self, 'Success', f'Matches have been downloaded to: {file_name}', QMessageBox.Ok)
         else:
